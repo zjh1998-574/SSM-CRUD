ce65a76dec6ed984a5adfcd07be1c086ee6edfe6
zjh1998-574 <73774783+zjh1998-574@users.noreply.github.com>
2021-11-03 21:17:11 +0800

    pull commit

--- src/main/java/com/cqupt/crud/test/Test.java
+++ src/main/java/com/cqupt/crud/test/Test.java
@@ -14,5 +14,6 @@ public class Test {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
